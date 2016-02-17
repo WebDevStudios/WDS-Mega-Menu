d8a71e4efa04b5fb2cf3fb0ba288d29bb623e9ef
Aubrey Portwood <aubreypwd@gmail.com>
2016-02-17 14:27:19 -0700

    We are not going to be able to use icons this way, remove this method
    To add icons we're going to have to use a library or add a way to filter
    in additional icons.

--- includes/class-wds-mega-menus-admin.php
+++ includes/class-wds-mega-menus-admin.php
@@ -18,17 +18,14 @@ if ( ! class_exists( 'WDS_Mega_Menus_Admin' ) ) {
 			add_filter( 'wp_setup_nav_menu_item', array( $this, 'register_nav_field' ) );
 			add_action( 'wp_update_nav_menu_item', array( $this, 'update_nav_fields'), 10, 3 );
 			add_filter( 'wp_edit_nav_menu_walker', array( $this, 'nav_menu_edit_walker' ) );
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
-			add_action( 'admin_print_scripts', array( $this, 'include_svg_definitions' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 		}
 
 		/**
 		 * Enqueue scripts.
 		 */
-		public function enqueue() {
-			$screen = get_current_screen();
-
-			if ( 'nav-menus'  !== $screen->id ) {
+		public function admin_enqueue_scripts() {
+			if ( 'nav-menus'  !== get_current_screen()->id ) {
 				return; // Only show on nav-menu's screen.
 			}
 
@@ -36,25 +33,6 @@ if ( ! class_exists( 'WDS_Mega_Menus_Admin' ) ) {
 			wp_enqueue_style( 'wdsmm-admin', plugins_url( '../assets/css/admin.css', __FILE__ ), array(), time() );
 			wp_enqueue_script( 'wds-mega-menus', plugins_url( '../assets/js/wds-mega-menus.js', __FILE__ ), array( 'jquery' ), time() );
 			wp_enqueue_script( 'bootstrap-dropdown', plugins_url( '../assets/js/dropdowns-enhancement.js', __FILE__ ), array( 'jquery' ), time(), true );
-		}
-
-
-		/**
-		 * Add SVG definitions to <head>.
-		 */
-		public function include_svg_definitions() {
-			$screen = get_current_screen();
-			if ( 'nav-menus'  !== $screen->id ) {
-				return;
-			}
-
-			// Define svg sprite file
-			$svg_defs = get_template_directory() . '/images/svg-defs.svg';
-
-			// If it exsists, include it
-			if ( file_exists( $svg_defs ) ) {
-				require_once( $svg_defs );
-			}
 		}
 
 		/**
