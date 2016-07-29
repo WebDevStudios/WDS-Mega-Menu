d99ecce42fab84af09b6f53446948c90c07f46bf
Chris Reynolds <chris@jazzsequence.com>
2016-07-29 09:04:39 -0600

    add svgmin and svgstore packages to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,37 @@ module.exports = function( grunt ) {
 	var pkg = grunt.file.readJSON( 'package.json' );
 	grunt.initConfig( {
 		pkg: pkg,
+		svgmin: {
+			options: {
+				plugins: [ // https://github.com/svg/svgo/tree/master/plugins
+					{ removeComments: true },
+					{ removeTitle: true },
+					{ removeEmptyAttrs: true }
+				]
+			},
+			dist: {
+				files: [{
+					expand: true,
+					cwd: '',
+					src: ['assets/svg/*.svg'],
+					dest: ''
+				}]
+			}
+		},
+		svgstore: {
+			options: {
+				prefix: 'icon-',
+				cleanup: ['fill', 'style'],
+				svg: {
+					style: 'display: none;'
+				}
+			},
+			default: {
+				files: {
+					'assets/svg-defs.svg': 'assets/svg/*.svg',
+				}
+			}
+		},
 		watch: {
 			php: {
 				files: ['**/*.php', '!vendor/**.*.php'],
@@ -33,11 +64,20 @@ module.exports = function( grunt ) {
 					}
 				}
 			}
-		}
+		},
+		svg: {
+			files: ['assets/svg/*.svg'],
+			tasks: ['svgstore'],
+			options: {
+				spawn: false,
+				livereload: true,
+			},
+		},
 	} );
 
 	// Default task.
 	grunt.registerTask( 'php', [ 'addtextdomain', 'makepot' ] );
+	grunt.registerTask('icons', ['svgmin', 'svgstore']);
 	grunt.registerTask( 'default', ['php'] );
 	grunt.util.linefeed = '\n';
 };
