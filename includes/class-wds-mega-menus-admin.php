aaf9d61e0a08055a41e75db677ec1410b84bf1b0
jomurgel <dev@jomurgel.com>
2016-09-30 10:25:09 -0700

    pull minified file
    wds-mega-menus.js

--- includes/class-wds-mega-menus-admin.php
+++ includes/class-wds-mega-menus-admin.php
@@ -38,7 +38,7 @@ if ( ! class_exists( 'WDS_Mega_Menus_Admin' ) ) {
 
 			wp_enqueue_media();
 			wp_enqueue_style( 'wdsmm-admin', wds_mega_menus()->url . 'assets/css/admin.css', array(), wds_mega_menus()->version );
-			wp_enqueue_script( 'wds-mega-menus', wds_mega_menus()->url . 'assets/js/wds-mega-menus.js', array( 'jquery' ), wds_mega_menus()->version );
+			wp_enqueue_script( 'wds-mega-menus', wds_mega_menus()->url . 'assets/js/wds-mega-menus.min.js', array( 'jquery' ), wds_mega_menus()->version );
 			wp_enqueue_script( 'bootstrap-dropdown', wds_mega_menus()->url . 'assets/js/dropdowns-enhancement.js', array( 'jquery' ), wds_mega_menus()->version, true );
 		}
 
